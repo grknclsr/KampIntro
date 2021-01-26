86a13d6723c94837af5a46203d33e230f6765402
GURKAN CALISIR <gurkan01c@gmail.com>
2021-01-26 16:40:55 +0300

    23 Ocak Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -19,6 +19,8 @@ namespace DegerVeReferansTipler
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]);
 
+
+
         }
     }
 }
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[]
+            {
+                "Engin","Murat", "Halil","Gürkan"
+            };
+
+            List<string> isimler2 = new List<string>() { "Murat", "Kerem", "Halil", "Gürkan" };
+
+            isimler2.Add("Ahmet");
+
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
